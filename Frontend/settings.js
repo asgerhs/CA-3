4215b8cf59a1045021033e8253c63b0493a42284
Asger H. Sørensen <znaszger@hotmail.com>
2019-11-12 16:04:17 +0100

    Update settings.js

--- Frontend/settings.js
+++ Frontend/settings.js
@@ -7,10 +7,12 @@ let URLs = new Map(
 function URLSettings() {
     const getURL = (key) => { URLs.get(key) }
     const getLink = (key) => { <Link to={URLs.get(key)}>{key}</Link> }
+    const getNavLink = (key) => { <NavnLink path={URLs.get(key)}>{key}</NavnLink> }
 
     return {
         getURL,
-        getLink
+        getLink,
+        getNavLink
     }
 }
 export default URLSettings();
