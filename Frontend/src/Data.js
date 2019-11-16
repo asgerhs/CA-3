0e7919a37507b9034e5dfa247db83e33947d32c7
MartinFrederiksen <zyjinxx@hotmail.com>
2019-11-16 14:34:10 +0100

    Update Data.js

--- Frontend/src/Data.js
+++ Frontend/src/Data.js
@@ -2,38 +2,23 @@ import React from 'react';
 import Facade from './login/ApiFacade';
 
 export default function Data() {
+  
     return (
       <div className="container">
-        <p><h3>Fetched data</h3></p>
+        <h3>Fetched data</h3>
         <table className="table">
           <thead className="thead-dark">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Height</th>
+              <th scope="col">Gender</th>
+              <th scope="col">url</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-            {/*{Facade.fetchData).then(data => <tr><td>Data.????</td><td>Data.????</td><td>Data.????</td><td>Data.????</td></tr>)} */}
+            <tr><td>{123}</td></tr>
+            {Facade.fetchData().then(data => data.map(p => console.log(p.name)))}
+            {/*<tr><td>{person.name}</td><td>{person.name}</td><td>{person.name}</td><td>{person.name}</td></tr> */}
           </tbody>
         </table>
       </div>
